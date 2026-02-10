ad4ea793fad8ebaf9d9160af3570c8123c053f7f
Mohammad Reza <mr.effaty@gmail.com>
2026-02-10 18:20:05 +0330

    User crud services

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -2,12 +2,55 @@ import { prisma } from "../../data/prisma";
 import { hashPassword } from "../../utils/hash";
 import type { RegisterUserRequest } from "./schemas/request";
 
-export const registerUser = async (body: RegisterUserRequest) => {
-  const { password, ...rest } = body;
+export const registerUser = async (input: RegisterUserRequest) => {
+  const { password, ...rest } = input;
   return prisma.user.create({
     data: {
       ...rest,
       passwordHash: await hashPassword(password),
+    },
+  });
+};
+
+export const getUserById = async (id: string) => {
+  return prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const getUserByEmail = async (email: string) => {
+  return prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+};
+
+export const updateUser = async (id: string, email?: string, name?: string) => {
+  const data: Record<string, string> = {};
+  if (email) {
+    data.email = email;
+  }
+  if (name) {
+    data.name = name;
+  }
+  return prisma.user.update({
+    where: {
+      id,
+    },
+    data,
+  });
+};
+
+export const changePassword = async (id: string, newPassword: string) => {
+  return prisma.user.update({
+    where: {
+      id,
+    },
+    data: {
+      passwordHash: await hashPassword(newPassword),
     },
   });
 };
