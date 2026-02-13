14428b12e58c9b91a736ff1738c54bf660337546
Mohammad Reza <mr.effaty@gmail.com>
2026-02-13 21:36:15 +0330

    create manyUsers query schema

---
+++ src/modules/users/schemas/query.ts
@@ -0,0 +1,21 @@
+import { z } from "zod/v4";
+
+export const userQuerySchemas = {
+  getManyUsers: z.object({
+    page: z.coerce
+      .number("Invalid number")
+      .int("Invalid int type")
+      .min(1, { error: "Could not be less than 1" })
+      .default(1),
+    limit: z.coerce
+      .number({ error: "Invalid number" })
+      .int({ error: "Invalid int type" })
+      .min(1, { error: "Could not be less than 1" })
+      .max(100, { error: "Could not be more than 100" })
+      .default(10),
+    search: z.string("Invalid string").optional(),
+    sort: z.enum(["createdAt_asc", "createdAt_desc"]).optional(),
+  }),
+};
+
+export type GetUserByIdQuery = z.infer<typeof userQuerySchemas.getManyUsers>;
