483ad15a794c4d9039706398563dd1cbf6ce85a5
Mohammad Reza <mr.effaty@gmail.com>
2026-02-19 19:55:37 +0330

    add docker compose

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,6 +4,10 @@ const registerRoutes = (app: FastifyInstance) => {
   app.register(import("../modules/users/user.route"), { prefix: "/users" });
   app.register(import("../modules/auth/auth.route"), { prefix: "/auth" });
   app.register(import("../modules/task/task.route"), { prefix: "/task" });
+
+  app.get("/helth-check", (_request: FastifyRequest, reply: FastifyReply) =>
+    reply.send({ message: "Ok" }),
+  );
 };
 
 export default registerRoutes;
